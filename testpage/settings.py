bcb213f58f3be3509f1f4394cde516080942eacf
Eimantas <eimantas.stonys@gmail.com>
2014-09-02 22:17:47 +0300

    removed db pw

--- testpage/settings.py
+++ testpage/settings.py
@@ -20,7 +20,7 @@ DATABASES = {
         'NAME': 'testdb',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
         'USER': 'eimantas',
-        'PASSWORD': 'maxmaxkit',
+        'PASSWORD': '',
         'HOST': 'localhost',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
         'PORT': '',                      # Set to empty string for default.
     }
